6ef306bd44254a5e7f027976f6f3da67ce09f52c
OneCrazyGenius <charlie.g.ensor@gmail.com>
2023-09-30 14:54:36 +0100

    spring boot -> tomcat container

---
+++ README.md
@@ -0,0 +1,9 @@
+## Commands
+### Docker
+- `docker-compose up --build` - Build and run the container
+- `docker-compose up` - Run the container
+- `docker-compose down` - Stop the container
+
+### Server (Java/Maven/Spring)
+- `mvn clean install` - Build the project
+- `mvn spring-boot:run` - Run the project
---
+++ server/src/main/java/dev/onecrazygenius/vitalityvibe/VitalityVibeApplication.java
@@ -0,0 +1,14 @@
+package dev.onecrazygenius.vitalityvibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class VitalityVibeApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitalityVibeApplication.class, args);
+	}
+
+}
