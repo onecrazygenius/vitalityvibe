689d758ccf9a818c9c71aad918668e7de376bc13
OneCrazyGenius <charlie.g.ensor@gmail.com>
2023-10-07 12:44:25 +0100

    basic post requests

--- server/src/main/java/dev/onecrazygenius/vitalityvibe/controllers/AuthController.java
+++ server/src/main/java/dev/onecrazygenius/vitalityvibe/controllers/AuthController.java
@@ -1,9 +1,12 @@
 package dev.onecrazygenius.vitalityvibe.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.onecrazygenius.vitalityvibe.models.UserModel;
 import dev.onecrazygenius.vitalityvibe.repository.UserRepository;
 
 @RestController
@@ -16,14 +19,40 @@ public class AuthController {
         this.userRepository = userRepository;
     }
 
-    @GetMapping("/login")
-    public String login() {
-        return "Login";
+    @PostMapping("/login")
+    public String login(@RequestBody UserModel user) {
+        UserModel foundUser = userRepository.findByEmail(user.getEmail());
+        if (foundUser != null) {
+            if (foundUser.getPassword().equals(user.getPassword())) {
+                // TODO: Generate JWT token
+                return "Login successful";
+            } else {
+                // TODO: Return error message
+                return "Incorrect password";
+            }
+        } else {
+            // TODO: Return error message
+            return "User not found";
+        }
     }
 
-    @GetMapping("/register")
-    public String register() {
-        return "Register";
+    @PostMapping("/register")
+    public String register(@RequestBody UserModel user) {
+        UserModel foundUser = userRepository.findByEmail(user.getEmail());
+        if (foundUser == null) {
+            userRepository.save(user);
+            // TODO: Return success message
+            return "Registration successful";
+        } else {
+            // TODO: Return error message
+            return "User already exists";
+        }
+    }
+
+    @GetMapping("/logout")
+    public String logout() {
+        // TODO: Invalidate JWT token
+        return "Logout successful";
     }
 }
 
--- server/src/main/java/dev/onecrazygenius/vitalityvibe/repository/UserRepository.java
+++ server/src/main/java/dev/onecrazygenius/vitalityvibe/repository/UserRepository.java
@@ -2,9 +2,13 @@ package dev.onecrazygenius.vitalityvibe.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.repository.Query;
+
 import dev.onecrazygenius.vitalityvibe.models.UserModel;
 
 @Repository
 public interface UserRepository extends MongoRepository<UserModel, String> {
+
+    @Query("{'email': ?0}")
     UserModel findByEmail(String email);
 }
