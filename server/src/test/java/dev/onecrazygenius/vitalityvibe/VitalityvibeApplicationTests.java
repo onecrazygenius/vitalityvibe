8ee5ce50647af108a30a513de5976a2463587fcf
OneCrazyGenius <charlie.g.ensor@gmail.com>
2023-10-10 22:41:25 +0100

    skip test

--- server/src/test/java/dev/onecrazygenius/vitalityvibe/VitalityvibeApplicationTests.java
+++ server/src/test/java/dev/onecrazygenius/vitalityvibe/VitalityvibeApplicationTests.java
@@ -3,7 +3,9 @@ package dev.onecrazygenius.vitalityvibe;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import dev.onecrazygenius.vitalityvibe.VitalityvibeApplication;
+
+@SpringBootTest(classes = VitalityvibeApplication.class)
 class VitalityvibeApplicationTests {
 
 	@Test
