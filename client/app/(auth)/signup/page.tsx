c9cfcc126c2593c2816554729fcd6fdf3a75b952
OneCrazyGenius <charlie.g.ensor@gmail.com>
2023-10-19 21:58:54 +0100

    fixed home button link

--- client/app/(auth)/signup/page.tsx
+++ client/app/(auth)/signup/page.tsx
@@ -37,7 +37,9 @@ export default function AuthenticationPage() {
           <div className="absolute inset-0 bg-zinc-900" />
           <div className="relative z-20 flex items-center text-lg font-medium">
             <Icons.logo className="mr-2 h-6 w-6" />
-            Vitality Vibe
+            <Link href="/" className="hover:text-gray-300">
+              Vitality Vibe
+            </Link>
           </div>
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
