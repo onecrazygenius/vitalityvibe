fa22b49150329d817ff44b0b428859dc7c74f6cd
OneCrazyGenius <charlie.g.ensor@gmail.com>
2023-10-21 16:06:58 +0100

    added session provider

--- client/app/providers.tsx
+++ client/app/providers.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { SessionProvider } from 'next-auth/react'
 import { ThemeProvider } from '@/providers/theme-provider'
 import { ToastProvider } from '@/providers/toast-provider'
 import { ModalProvider } from '@/providers/modal-provider'
@@ -10,14 +11,16 @@ export function Providers({
     children: React.ReactNode,
 }) {
     return (
-        <ThemeProvider
-            attribute='class'
-            defaultTheme='dark'
-            enableSystem
-        >
-            <ToastProvider />
-            <ModalProvider />
-            {children}
-        </ThemeProvider>
+        <SessionProvider>
+            <ThemeProvider
+                attribute='class'
+                defaultTheme='dark'
+                enableSystem
+            >
+                <ToastProvider />
+                <ModalProvider />
+                {children}
+            </ThemeProvider>
+        </SessionProvider>
     )
 }
