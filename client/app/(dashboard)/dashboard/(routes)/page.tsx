41d3b40181e114eaf6f7e7cc9768362c7d58293b
OneCrazyGenius <charlie.g.ensor@gmail.com>
2023-10-21 14:31:49 +0100

    goals achieved

--- client/app/(dashboard)/dashboard/(routes)/page.tsx
+++ client/app/(dashboard)/dashboard/(routes)/page.tsx
@@ -16,6 +16,24 @@ import { CalendarDateRangePicker } from '@/components/ui/date-range-picker';
 const DashboardPage = () => {
     const { theme } = useTheme();
 
+    const goals = [
+        {
+            title: "Sleep",
+            value: "7.5hrs",
+            target: "8hrs",
+        },
+        {
+            title: "Exercise",
+            value: "2.5hrs",
+            target: "3hrs",
+        },
+        {
+            title: "Meals",
+            value: "2100kcal",
+            target: "2000kcal",
+        },
+    ];
+
     return ( 
         <PageTransition>
             <div className="flex-1 space-y-4 p-8 pt-6">
@@ -98,6 +116,56 @@ const DashboardPage = () => {
                             </p>
                         </CardContent>
                     </Card>
+                </div>
+                <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-12">
+                    <div className="col-span-12 md:col-span-1 lg:col-span-5">
+                        <Card>
+                            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                <CardTitle className="text-sm font-medium">
+                                    Goals
+                                </CardTitle>
+                                <Icons.goals className="h-4 w-4 text-gray-500" />
+                            </CardHeader>
+                            <CardContent>
+                                <div className="text-2xl font-bold text-white mb-4">
+                                    Achieved 2/3 goals
+                                </div>
+                                {goals.map((goal, index) => (
+                                    <div
+                                        key={index}
+                                        className={cn(
+                                            "flex flex-row items-center justify-between",
+                                            index !== goals.length - 1
+                                                ? "pb-2 mb-2 border-b border-gray-200"
+                                                : ""
+                                        )}
+                                    >
+                                        <div className="flex flex-row items-center space-x-2">
+                                            <Icons.apple className="h-4 w-4 text-gray-500" />
+                                            <div className="text-sm font-medium text-gray-700">
+                                                {goal.title}
+                                            </div>
+                                        </div>
+                                        <div className="text-sm font-medium text-gray-700">
+                                            {goal.value} / {goal.target}
+                                        </div>
+                                    </div>
+                                ))}
+                            </CardContent>
+                        </Card>
+                    </div>
+                    <div className="col-span-12 md:col-span-1 lg:col-span-7">
+                        <Card>
+                            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                <CardTitle className="text-sm font-medium">
+                                    Circles
+                                </CardTitle>
+                                <Icons.circles className="h-4 w-4 text-gray-500" />
+                            </CardHeader>
+                            <CardContent>
+                            </CardContent>
+                        </Card>
+                    </div>
                 </div>
             </div>
         </PageTransition>
