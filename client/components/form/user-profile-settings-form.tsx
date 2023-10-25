720d96506daf65346b6871861b027667732999f5
OneCrazyGenius <charlie.g.ensor@gmail.com>
2023-10-25 20:58:19 +0100

    removed session update

--- client/components/form/user-profile-settings-form.tsx
+++ client/components/form/user-profile-settings-form.tsx
@@ -97,9 +97,6 @@ export default function ProfileForm() {
         return;
       }
 
-      // Update the session with the new user data
-      update();
-
       setIsLoading(false);
       toast({
         title: "Profile Updated",
