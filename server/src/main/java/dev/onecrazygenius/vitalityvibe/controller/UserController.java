7fd31c582231f2ff92ca1a4e6ab798d50a0ecfcb
OneCrazyGenius <charlie.g.ensor@gmail.com>
2023-10-25 20:47:36 +0100

    improved security and structure of api controllers

--- server/src/main/java/dev/onecrazygenius/vitalityvibe/controller/UserController.java
+++ server/src/main/java/dev/onecrazygenius/vitalityvibe/controller/UserController.java
@@ -5,10 +5,15 @@ import dev.onecrazygenius.vitalityvibe.service.UserServiceImpl;
 import dev.onecrazygenius.vitalityvibe.payload.request.UserRequest;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -23,75 +28,91 @@ import org.slf4j.LoggerFactory;
 @RequestMapping("/api/user")
 public class UserController {
 
-	private static Logger logger = LoggerFactory.getLogger(UserController.class);
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     private UserServiceImpl service;
 
-    @GetMapping("/profile")  
+    @GetMapping("/profile")
     @PreAuthorize("hasAuthority('ROLE_USER')")
-	public ResponseEntity<?> userProfile() { 
+    public ResponseEntity<?> userProfile() {
         logger.info("User profile request");
-		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        
-        if (principal instanceof UserDetails) {
-			UserDetails userDetails = (UserDetails) principal;
 
-			// Attempt to extract the email from the user details
-			String email = userDetails.getUsername();
-			// Add some logging to help diagnose the issue
-			logger.info("Extracted email: " + email);
+        try {
+            Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-			// Get user by email, if user exists
-			User user = service.getUserByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found " + email));
+            if (principal instanceof UserDetails) {
+                UserDetails userDetails = (UserDetails) principal;
+                String email = userDetails.getUsername();
 
-			// Return the user profile as a map
-			Map<String, Object> profile = new HashMap<>();
-			profile.put("name", user.getName());
-			profile.put("email", user.getEmail());
-			profile.put("bio", user.getBio());
-			profile.put("roles", user.getRoles());
-			return ResponseEntity.ok(profile);
-		} else {
-			// Handle the case where the principal is not of the expected type
-			logger.warn("Invalid user profile request");
-			return ResponseEntity.badRequest().body("Invalid user profile request");
-		}
-	} 
+                if (isValidEmail(email)) {
+                    User user = service.getUserByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found " + email));
 
-	@PatchMapping("/profile")
-	@PreAuthorize("hasAuthority('ROLE_USER')")
-	public ResponseEntity<?> updateUserProfile(@RequestBody UserRequest user) {
-		logger.info("User profile update request");
+                    if (isValidRoles(user.getRoles())) {
+                        Map<String, Object> profile = new HashMap<>();
+                        profile.put("name", user.getName());
+                        profile.put("email", user.getEmail());
+                        profile.put("bio", user.getBio());
+                        profile.put("roles", user.getRoles());
+                        return ResponseEntity.ok(profile);
+                    }
+                }
+            }
 
-		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		
-		if (principal instanceof UserDetails) {
-			UserDetails userDetails = (UserDetails) principal;
+            logger.warn("Invalid user profile request");
+            return ResponseEntity.badRequest().body("Invalid user profile request");
+        } catch (Exception e) {
+            logger.error("Error in user profile request: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred");
+        }
+    }
 
-			// Attempt to extract the email from the user details
-			String email = userDetails.getUsername();
-			// Add some logging to help diagnose the issue
-			logger.info("Extracted email: " + email);
+    @PatchMapping("/profile")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public ResponseEntity<?> updateUserProfile(@RequestBody UserRequest user) {
+        logger.info("User profile update request");
 
-			// Get user by email, if user exists
-			User existingUser = service.getUserByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found " + email));
+        try {
+            Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-			// Update the user profile
-			existingUser.setName(user.getName());
-			existingUser.setEmail(user.getEmail());
-			existingUser.setBio(user.getBio());
+            if (principal instanceof UserDetails) {
+                UserDetails userDetails = (UserDetails) principal;
+                String email = userDetails.getUsername();
 
-			// Save the updated user profile
-			service.updateUser(existingUser);
+                if (isValidEmail(email)) {
+                    User existingUser = service.getUserByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found " + email));
 
-			// Return happy
-			return ResponseEntity.ok("User profile updated successfully");
-		} else {
-			// Handle the case where the principal is not of the expected type
-			logger.warn("Invalid user profile update request");
-			return ResponseEntity.badRequest().body("Invalid user profile update request");
-		}
-	}
+                    if (isValidName(user.getName()) && isValidEmail(user.getEmail())) {
+                        existingUser.setName(user.getName());
+                        existingUser.setEmail(user.getEmail());
+                        existingUser.setBio(user.getBio());
 
+                        service.updateUser(existingUser);
+
+                        return ResponseEntity.ok("User profile updated successfully");
+                    }
+                }
+            }
+
+            logger.warn("Invalid user profile update request");
+            return ResponseEntity.badRequest().body("Invalid user profile update request");
+        } catch (Exception e) {
+            logger.error("Error in user profile update request: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred");
+        }
+    }
+
+    // Helper methods for validation
+    private boolean isValidEmail(String email) {
+        return email.matches("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
+    }
+
+    private boolean isValidName(String name) {
+        return name.matches("^[a-zA-Z\\s]*$");
+    }
+
+    private boolean isValidRoles(String roles) {
+		List<String> validRoles = Arrays.asList("ROLE_USER", "ROLE_ADMIN");
+		return validRoles.contains(roles);
+	}	
 }
